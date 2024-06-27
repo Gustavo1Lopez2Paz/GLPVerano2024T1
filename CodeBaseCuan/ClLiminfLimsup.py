997c43f395c4da7ac8ce780f639acf375283dcf3
Gustavo Lopez Paz <230110531@itsoeh.edu.mx>
2024-06-27 08:42:09 -0600

    Ejercicio 8: seccion a
    Signed-off-by: Gustavo Lopez Paz <230110531@itsoeh.edu.mx>

--- CodeBaseCuan/ClLiminfLimsup.py
+++ CodeBaseCuan/ClLiminfLimsup.py
@@ -15,3 +15,18 @@ def clases_grouped(datos):
 
     return lim_inf, lim_sup, marc_clase
 
+def clases_grouped1(datos):
+    amplitud = max(datos) - min(datos)
+    nclases = 1 + (3.3*math.log10(len(datos)))
+    anc_clas = round(amplitud / math.floor(nclases), 2)
+
+    marc_clase = []
+    lim_inf = []
+    lim_sup = []
+
+    for i in range(int(nclases)):
+        marc_clase.append(round(min(datos) + i*anc_clas + anc_clas/2, 3))
+        lim_inf.append(round(min(datos) + i*anc_clas, 2))
+        lim_sup.append(round(min(datos) + (i+1)*anc_clas, 2))
+
+    return lim_inf, lim_sup, marc_clase
